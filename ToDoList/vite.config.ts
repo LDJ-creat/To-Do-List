53ab66e9a9f96d0fb845b11becf5dd2cd2bbba13
LDJ <2271115019@qq.com>
2024-11-12 12:01:53 +0800

    build github pages

--- ToDoList/vite.config.ts
+++ ToDoList/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"./",
   // plugins: [react[include:'**/*.svg']],
   plugins: [react({
     include: '**/*.svg' // 改为使用对象的方式
